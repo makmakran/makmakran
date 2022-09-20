326cfb5af1258e1f7011ebd774849ad2b92fd9f1
makmak <100479278+makmakran@users.noreply.github.com>
2022-09-20 23:41:27 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import base64
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 import numpy as np
 
 st.title('Next Best Action Prediction - Invest in the right opportunities')
