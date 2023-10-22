d4854772c4371aaf8b834fff116dcfa114f15513
makmak <100479278+makmakran@users.noreply.github.com>
2023-10-22 20:41:35 +0300

    myscore2.py

--- myscore2.py
+++ myscore2.py
@@ -60,22 +60,13 @@ with st.form("data_editor_form"):
     #df.style.highlight_max(color = 'lightgreen', axis = 0)    
     submit_button = st.form_submit_button("Submit")
 
-    if save_button:
-        try:
-            # Convert the edited DataFrame to CSV using pandas
-            edited.to_csv("myscores2.csv", index=False)
-            st.success("Changes saved to CSV file")
-        except Exception as e:
-            st.error(f"An error occurred while saving changes: {e}")
-
-
     
-    # edited_df = st.experimental_data_editor(
-        # df,
-        # use_container_width=True,
-        # num_rows="dynamic",
-    #df = df.append(df, ignore_index=True)
-    # st.write(df)
+     edited_df = st.experimental_data_editor(
+         df,
+         use_container_width=True,
+         num_rows="dynamic",
+    df = df.append(df, ignore_index=True)
+     st.write(df)
     edited.to_csv("myscores2.csv", index=False)
     st.caption("Modify cells above 👆 or even ➕ add rows, reload to check 👇")
     
